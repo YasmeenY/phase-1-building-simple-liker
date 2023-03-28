aeb7c00d7069749993330315c10ae210c061330f
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-03-28 10:38:18 -0400

    complete lab

--- main.js
+++ main.js
@@ -2,10 +2,32 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const heartSign = document.getElementsByClassName("like-glyph");
 
+function likeFunc(e){
+  const target = e.target;
+  mimicServerCall("http://mimicServer.example.com")
+  .then(function(){
+    if (target.textContent === EMPTY_HEART){
+      target.textContent = FULL_HEART;
+      target.className = "active-heart";
+    }
+    else {
+      target.textContent = EMPTY_HEART;
+      target.className = "";
+    }
+  })
+  .catch(function(error){
+    const modal = document.getElementById("modal");
+    modal.className = "";
+    modal.textContent = error;
+    setTimeout(() => modal.className = "hidden", 3000)
+  })
+}
 
-
+for (const glyph of heartSign){
+  glyph.addEventListener("click", likeFunc)
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
